2f0a2d2e175fcb2591c2fccd6878f350b5bfd4b6
Mario Bodemann <mario.bodemann@gmail.com>
2019-03-18 11:53:55 +0100

    add help option and create settings

--- src/main/java/net/karmacoder/duke/Main.java
+++ src/main/java/net/karmacoder/duke/Main.java
@@ -7,53 +7,68 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static net.karmacoder.duke.samples.Images.duke;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    if (args.length == 0) {
-      showHelp();
-    } else {
-      boolean scaled = false;
-      float scale = 1.0f;
-      int width = -1;
-      int height = -1;
+  public static class Settings {
+    boolean scaled = false;
+    float scale = 1.0f;
+    int width = -1;
+    int height = -1;
+    List<String> files = new ArrayList<>();
 
-      for (int i = 0; i < args.length; ++i) {
-        final String option = args[i];
-        if (option.equals("--scale") && args.length > i + 1) {
-          final String stringSize = args[i + 1];
-          scale = Float.parseFloat(stringSize);
-          scaled = true;
+    static Settings fromArguments(List<String> arguments) {
+      final Settings settings = new Settings();
+      for (int i = 0; i < arguments.size(); ++i) {
+        final String argument = arguments.get(i);
+        if (argument.equals("--scale") && arguments.size() > i + 1) {
+          final String stringSize = arguments.get(i + 1);
+          settings.scale = Float.parseFloat(stringSize);
+          settings.scaled = true;
         }
 
-        if (option.equals("--width") && args.length > i + 1) {
-          final String stringWidth = args[i + 1];
-          width = Integer.parseInt(stringWidth);
+        if (argument.equals("--width") && arguments.size() > i + 1) {
+          settings.width = Integer.parseInt(arguments.get(i + 1));
         }
 
-        if (option.equals("--height") && args.length > i + 1) {
-          final String stringheight = args[i + 1];
-          height = Integer.parseInt(stringheight);
+        if (argument.equals("--height") && arguments.size() > i + 1) {
+          settings.height = Integer.parseInt(arguments.get(i + 1));
+        }
+
+        if (new File(argument).exists()) {
+          settings.files.add(argument);
         }
       }
 
-      final List<String> files = Arrays.stream(args).filter(it -> new File(it).exists()).collect(Collectors.toList());
-      for (final String file : files) {
-        if (files.size() > 1) {
+      return settings;
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    final List<String> arguments = Arrays.asList(args);
+
+    final Settings settings = Settings.fromArguments(arguments);
+
+    if (arguments.size() == 0 || arguments.contains("--help") || arguments.contains("-h") || settings.files.size() == 0) {
+      showHelp();
+    } else {
+      for (final String file : settings.files) {
+        if (settings.files.size() > 1) {
           System.out.println(file);
         }
 
         final BufferedImage img = ImageIO.read(new File(file));
 
-        if (scaled) {
-          new Console().display(Images.fromBufferedImage(img, scale));
+        if (settings.scaled) {
+          new Console().display(Images.fromBufferedImage(img, settings.scale));
+        } else if (settings.width > 0 || settings.height > 0) {
+          new Console().display(Images.fromBufferedImage(img, settings.width, settings.height));
         } else {
-          new Console().display(Images.fromBufferedImage(img, width, height));
+          new Console().display(Images.fromBufferedImage(img));
         }
       }
     }
@@ -64,8 +79,8 @@ public class Main {
     System.out.println();
     System.out.println("Console image displayer: Display an image in the console");
     System.out.println();
-    System.out.println("Usage: duke [--scale S=1.0] [--width W=-1] [--height H=-1] FILE_1 [.. FILE_N]");
+    System.out.println("Usage: duke [-h|--help] [--scale S=1.0] [--width W=-1] [--height H=-1] [FILE_1 [.. FILE_N]]");
     System.out.println();
-    System.out.println("\tDisplay files 1 to N in console. If scale s is given, rescale all images by factor S.");
+    System.out.println("\tDisplay image files 0 to N in console. If scale s is given, rescale all images by factor S.");
   }
 }
